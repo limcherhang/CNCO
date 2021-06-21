b7502f700aa2f9be4d0abbf8c4c5e2b3c6f15202
LIM CHER HANG <79460375+limcherhang@users.noreply.github.com>
2021-06-22 00:30:44 +0800

    Add files via upload

---
+++ Conopt/experiments/celebA/simgd.py
@@ -0,0 +1,42 @@
+import os
+from subprocess import call
+from os import path
+import sys
+
+# Executables
+executable = 'python'
+
+# Paths
+rootdir = '../..'
+scriptname = 'run.py'
+cwd = os.path.dirname(os.path.abspath(__file__))
+outdir = os.path.join(cwd, 'out/simgd')
+
+args = [
+# Architecture
+'--image-size', '160',
+'--output-size', '64',
+'--c-dim', '3',
+'--z-dim', '256',
+'--gf-dim', '128',
+'--df-dim', '128',
+'--g-architecture', 'resnet_cf',
+'--d-architecture', 'resnet_cf',
+'--gan-type','standard',
+# Training
+'--optimizer', 'simgd',
+'--nsteps', '150000',
+'--ntest', '1000',
+'--learning-rate', '1e-4',
+'--batch-size', '128',
+'--log-dir', os.path.join(outdir, 'tf_logs'),
+'--sample-dir', os.path.join(outdir, 'samples'),
+# Data set
+'--dataset', 'celebA',
+'--data-dir', './data',
+'--split', 'train'
+]
+
+# Run
+my_env = os.environ.copy()
+call([executable, scriptname] + args, env=my_env, cwd=rootdir)
